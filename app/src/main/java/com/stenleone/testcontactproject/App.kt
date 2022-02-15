82d4d9d39b7c59d68a0aa14fec7419d4b8c8f19e
StanleyDuFoster <stenleone@gmail.com>
2022-02-15 22:59:39 +0200

    add libs

--- app/src/main/java/com/stenleone/testcontactproject/App.kt
+++ app/src/main/java/com/stenleone/testcontactproject/App.kt
@@ -1,5 +1,21 @@
 package com.stenleone.testcontactproject
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
-class App : Application()
+@HiltAndroidApp
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+
+}
--- app/src/main/java/com/stenleone/testcontactproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/stenleone/testcontactproject/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.stenleone.testcontactproject
+package com.stenleone.testcontactproject.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stenleone.testcontactproject.R
 
 class MainActivity : AppCompatActivity() {
 
